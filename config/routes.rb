4ad79bbf7dfab648e68cd423779b6a7424bbbb9e
Steven Spiller <stevenspiller@bittheory.biz>
2016-08-11 15:54:47 +0100

    Shopify connection and related UI

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,12 @@ Contestapp::Application.routes.draw do
   get "dashboard/index"
   root "dashboard#index"
 
+  resources :accounts do
+    member do
+      get "test_connection"
+    end
+  end
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
